ead9443b6e44537df7111052a2368d70b42d7c4b
JustinGibbon <justingibbon2@gmail.com>
2024-05-27 10:16:26 +0200

    fixign final errors with on click function

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 let movieSearchBox =document.getElementById('movie_search_box');
 let searchList =document.getElementById('search_list');
 let resultGrid =document.getElementById('result_grid');
-
+//creating a function to fetch the movies from the ombd api
 async function loadMovies(searchTerm){
     const URL = `https://omdbapi.com/?s=${searchTerm}&page=1&apikey=fc1fef96`;
     const res = await fetch(`${URL}`);
@@ -15,17 +15,19 @@ async function loadMovies(searchTerm){
 }
 //testing function
 //loadMovies('Spiderman');
+//funvtion that searches the movie and implements the hide_search_list function when clicking on the seach bar
 function findMovies(){
     let searchTerm = (movieSearchBox.value).trim();
     //console.log(searchTerm);
     if (searchTerm.length >0){
-        searchList.classList.remove('hide_search-list');
+        searchList.classList.remove('hide_search_list');
         loadMovies(searchTerm);
     }else{
-        searchList.classList.add('hide_search-list');
+        searchList.classList.add('hide_search_list');
     }
 
 }
+//this function will be run when selecting a film in order to display it. IF there is no poster an default placeholder image will be used
 
 function displayMovieList(movies){
     searchList.innerHTML = "";
@@ -38,7 +40,7 @@ function displayMovieList(movies){
             moviePoster= movies[idx].Poster;
         else
         moviePoster= "imagenotfound.jpg";
-
+        //adding htmm and replacing values with our variables from the omdb api
         movieListItem.innerHTML = `
         <div class="search_list_thumb">
             <img src ="${moviePoster}">
@@ -57,18 +59,21 @@ function loadMovieDetails(){
     const searchListMovies = searchList.querySelectorAll('.search_list_item');
     searchListMovies.forEach(movie => {
         movie.addEventListener('click', async () => {
+            //the console.log was used to see if the films get loaded
             // console.log(movie.dataset.id);
             searchList.classList.add('hide_search_list');
             movieSearchBox.value = "";
             const result = await fetch(`http://www.omdbapi.com/?i=${movie.dataset.id}&apikey=424184a5`);
             const movieDetails = await result.json();
             // console.log(movieDetails);
+            //once again console.log was used to see if the movie details get printed in the cosole.
             displayMovieDetails(movieDetails);
         });
     });
 }
 
 function displayMovieDetails(details){
+    //adding htmm and replacing movie details with our variables from the omdb api
     resultGrid.innerHTML =`<div class = "movie_poster">
     <img src=${(details.Poster != "N/A") ? details.Poster : "image_not_found.png"}" alt= "Movie_Poster">
 </div>
@@ -87,6 +92,7 @@ function displayMovieDetails(details){
     <p class = "awards"><b><i class = "fas fa-award"></i></b> ${details.Awards}</p>
 </div>`
 }
+//this event listenr will be our on click function to hide or display the search bar.
 window.addEventListener('click', (event) => {
     if(event.target.className != "form_control"){
         searchList.classList.add('hide_search_list');
